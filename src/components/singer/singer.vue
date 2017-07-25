8c997ef0c102b8c876c3d609d4dbf60c945f42cf
zhaobao1830 <x5219438>
2017-07-26 01:49:10 +0800

    歌手数据处理

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -5,6 +5,11 @@
 <script type="text/ecmascript-6">
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  const HOT_NAME = '热门'
+  const HOT_SINGER_LEN = 10
+
   export default {
     data () {
       return {
@@ -18,7 +23,37 @@
       _getSingerList: function () {
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
-//            this.singers =
+            this.singers = res.data.list
+            console.log(this._normalizeSinger(this.singers))
+          }
+        })
+      },
+      _normalizeSinger: function (list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        console.log(list)
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+            console.log(map[key])
+            map[key].items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
           }
         })
       }
