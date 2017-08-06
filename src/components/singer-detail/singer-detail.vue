20527c8dccc5152a57e9fe1ecaff3039425ef931
zhaobao1830 <x5219438>
2017-08-07 00:37:57 +0800

    music-list组件开发3

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="slide">
-    <div class="singer-detail"></div>
+    <music-list :songs="songs" :title="title" :bgImage="bgImage"></music-list>
     <router-view></router-view>
   </transition>
 </template>
@@ -10,9 +10,16 @@
   import {ERR_OK} from 'api/config'
   import {getSingerDetail} from 'api/singer'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
 
   export default {
     computed: {
+      title () {
+        return this.singer.name
+      },
+      bgImage () {
+        return this.singer.avatar
+      },
       ...mapGetters([
         'singer'
       ])
@@ -34,7 +41,6 @@
         getSingerDetail(this.singer.id).then((res) => {
           if (res.code === ERR_OK) {
             this.songs = this._normalizeSongs(res.data.list)
-            console.log(this.songs)
           }
         })
       },
@@ -48,6 +54,9 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
@@ -55,14 +64,6 @@
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
   @import "~common/stylus/variable"
 
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-background
   .slide-enter-active,.slide-leave-active
     transition: all .3s
   .slide-enter,.slide-leave-to
