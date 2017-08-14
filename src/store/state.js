6b20437dca4f6f65f62c56e5f446b267dc51b20d
zhaobao1830 <x5219438>
2017-08-14 22:17:33 +0800

    播放器vuex数据设计

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,15 @@
 /**
  * 状态管理
  */
+import {playMode} from 'common/js/config'
+
 const state = {
-  singer: {}
+  singer: {},
+  playing: false, // 播放
+  fullScreen: false, // 全屏
+  playList: [], // 播放列表
+  sequenceList: [], // 顺序列表
+  mode: playMode.sequence, // 播放状态
+  currentIndex: -1 // 当前播放的索引
 }
 export default state
