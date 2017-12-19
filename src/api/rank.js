79e5ae6d2ffb7227252784bea2973a12c285d31a
zhaobao1830 <x5219438>
2017-12-19 22:05:23 +0800

    排行页数据应用

--- src/api/rank.js
+++ src/api/rank.js
@@ -1,7 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
-export function getTopList() {
+export function getTopList () {
   const url = 'https://c.y.qq.com/v8/fcg-bin/fcg_myqq_toplist.fcg'
 
   const data = Object.assign({}, commonParams, {
--- src/components/rank/rank.vue
+++ src/components/rank/rank.vue
@@ -2,14 +2,14 @@
   <div class="rank" ref="rank">
     <scroll :data="topList" class="toplist" ref="toplist">
       <ul>
-        <li class="item">
+        <li @click="selectItem(item)" class="item" v-for="item in topList">
           <div class="icon">
-            <img width="100" height="100"/>
+            <img width="100" height="100" v-lazy="item.picUrl"/>
           </div>
           <ul class="songlist">
-            <li class="song">
-              <span></span>
-              <span></span>
+            <li class="song" v-for="(song, index) in item.songList">
+              <span>{{index + 1}}</span>
+              <span>{{song.songname}}-{{song.singername}}</span>
             </li>
           </ul>
         </li>
@@ -26,9 +26,11 @@
   import Scroll from 'base/scroll/scroll'
   import Loading from 'base/loading/loading'
   import {getTopList} from 'api/rank'
+  import {playlistMixin} from 'common/js/mixin'
   import {ERR_OK} from 'api/config'
 
   export default {
+    mixins: [playlistMixin],
     created () {
       this._getTopList()
     },
@@ -38,6 +40,11 @@
       }
     },
     methods: {
+      handlePlaylist (playlist) {
+        const bottom = playlist.length > 0 ? '60px' : ''
+        this.$refs.rank.style.bottom = bottom
+        this.$refs.topList.refresh()
+      },
       _getTopList () {
         getTopList().then((res) => {
           if (res.code === ERR_OK) {
@@ -54,5 +61,45 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus" type="text/stylus">
+  @import "~common/stylus/variable"
+  @import "~common/stylus/mixin"
 
+  .rank
+    position: fixed
+    width: 100%
+    top: 88px
+    bottom: 0
+    .toplist
+      height: 100%
+      overflow: hidden
+      .item
+        display: flex
+        margin: 0 20px
+        padding-top: 20px
+        height: 100px
+        &:last-child
+          padding-bottom: 20px
+        .icon
+          flex: 0 0 100px
+          width: 100px
+          height: 100px
+        .songlist
+          flex: 1
+          display: flex
+          flex-direction: column
+          justify-content: center
+          padding: 0 20px
+          height: 100px
+          overflow: hidden
+          background: $color-highlight-background
+          color: $color-text-d
+          font-size: $font-size-small
+          .song
+            no-wrap()
+            line-height: 26px
+      .loading-container
+        position: absolute
+        width: 100%
+        top: 50%
+        transform: translateY(-50%)
 </style>
